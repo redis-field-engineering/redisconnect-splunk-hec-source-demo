f443a05dc82d0f7fd9a3ff93d61775133e9df7da
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2022-11-14 22:47:31 -0500

    Demo client updates

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # redisconnect-splunk-hec-source-demo
+Sample client (source) to send log messages (Java logging using logback) to Splunk HTTP Event Collector (HEC)
+
+## Download
+
+Download the [latest release](https://github.com/redis-field-engineering/redisconnect-splunk-hec-source-demo/releases) and un-tar (e.g. tar -xvf redisconnect-splunk-hec-source-1.0-SNAPSHOT.tar.gz) the redisconnect-splunk-hec-source-1.0-SNAPSHOT.tar.gz archive.
+
+## Usage
+
+```bash
+java -jar \
+-Dlogback.configurationFile=config/logback.xml \
+-Dsplunk.url=<HEC_ENDPOINT> \
+-Dsplunk.token=<HEC_TOKEN> \
+-Dsplunk.index=<HEC_INDEX_NAME> \
+-Diter=1 \
+redisconnect-splunk-hec-source-1.0-SNAPSHOT.jar
+```
--- src/main/java/com/redis/connect/splunk/HecProducer.java
+++ src/main/java/com/redis/connect/splunk/HecProducer.java
@@ -2,33 +2,27 @@ package com.redis.connect.splunk;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import picocli.CommandLine;
 
+import java.time.Instant;
 import java.util.Date;
+import java.util.UUID;
 
-@CommandLine.Command(name = "com.redis.connect.splunk.HecProducer", usageHelpAutoWidth = true, description = "HEC producer load generator.")
 public class HecProducer implements Runnable {
 
     private static final Logger logger = LoggerFactory.getLogger(HecProducer.class.getName());
-    @CommandLine.Option(names = {"-i", "--iter"}, description = "Iterations to run (default: ${DEFAULT-VALUE})", defaultValue = "1")
-    private static int iteration;
-
-    public HecProducer(int iter) {
-        HecProducer.iteration = iter;
-    }
+    private static final int iteration = Integer.parseInt(System.getProperty("iter", String.valueOf(1)));
 
     public static void main(String[] args) {
-        new CommandLine(new HecProducer(iteration)).execute(args);
+        new HecProducer().run();
     }
 
     @Override
     public void run() {
 
-        Date date = new Date();
-        long now = System.currentTimeMillis();
         for (int i = 1; i <= iteration; i++) {
-            String jsonMsg = String.format("{EventDate:'%s', EventTimeInMillis:'%d', EventMsg:'This is test event %d for Java Logging with Logback and Splunk HEC'}", date, now, i);
+            String jsonMsg = String.format("{EventId:'%s', EventDate:'%s', EventTimeInMillis:'%d', EventMsg:'This is test event %d for Java Logging with Logback and Splunk HEC'}", UUID.randomUUID(), new Date(), Instant.now().toEpochMilli(), i);
             logger.info("{}", jsonMsg);
+            logger.debug("Splunk URL: {}, Splunk Token: {}, Splunk Index: {}", System.getProperty("splunk.url"), System.getProperty("splunk.token"), System.getProperty("splunk.index"));
         }
 
         try {
